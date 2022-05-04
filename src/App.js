cd259bf07bca4e33f7179a300deeb23ad734f531
Anthony4m <amoahanthony41@gmail.com>
2022-05-04 13:57:25 +0000

    Testing second build

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React app
+          Testing hello world
         </p>
         <a
           className="App-link"
